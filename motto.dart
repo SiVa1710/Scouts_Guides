41d6d8a754ba0f201280adb01f8615fd51924c13
SIVASUBRAMANIAN R <54441582+SiVa1710@users.noreply.github.com>
2024-04-14 18:47:31 +0530

    Create motto.dart

---
+++ motto.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+class Motto extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'MOTTO',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            fontFamily: 'Sarabun',
+            letterSpacing: 1.5,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Color(0xFF0001cf),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(screenWidth * 0.03),
+        children: [
+          buildMottoItem(
+            mottoName: 'TAIYAR (BE PREPARED)',
+            mottoDescription: MottoText,
+            screenWidth: screenWidth,
+            screenHeight: screenHeight,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildMottoItem({
+    String mottoName,
+    String mottoDescription,
+    double screenWidth,
+    double screenHeight,
+  }) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Card(
+          elevation: 2,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          color: Color(0xFF0001cf),
+          child: ListTile(
+            leading: Image.asset(
+              'assets/icons/motto.png', // Replace with your image asset path
+              width: 30, // Adjust image size
+              height: 30,
+            ),
+            title: Text(
+              mottoName,
+              style: TextStyle(
+                fontSize: screenWidth * 0.05,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: screenHeight * 0.02),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 24.0),
+                child: RichText(
+                  text: TextSpan(
+                    children: mottoDescription.split('#').map((line) {
+                      return TextSpan(
+                        text: line,
+                        style: TextStyle(
+                          fontSize: screenWidth * 0.048,
+                          fontFamily: 'Lora',
+                          fontWeight: FontWeight.w900,
+                          letterSpacing: 1.5,
+                          color: Color(0xFF0001cf),
+                          height: 1.5,
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+final String MottoText = '''
+#• Physically strong
+#• Morally straight 
+#• Mentally awake
+''';
+
+void main() {
+  runApp(MaterialApp(
+    home: Motto(),
+  ));
+}
