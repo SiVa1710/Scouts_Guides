e490b43a73521cd4cc24254dac64192f2d34a0bf
SIVASUBRAMANIAN R <54441582+SiVa1710@users.noreply.github.com>
2024-04-26 13:21:37 +0530

    Update exercises.dart

--- exercises.dart
+++ exercises.dart
@@ -1,9 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+const TextStyle flagTitleStyle = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+);
+
+const TextStyle flagDescriptionStyle = TextStyle(
+  fontSize: 18.0,
+  fontFamily: 'Lora',
+  fontWeight: FontWeight.w900,
+  letterSpacing: 1.5,
+  color: Color(0xFF0001cf),
+  height: 1.5,
+);
+
+void main() {
+  runApp(MaterialApp(
+    home: Exercises(),
+  ));
+}
 
 class Exercises extends StatelessWidget {
   @override
-
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -19,65 +48,67 @@ class Exercises extends StatelessWidget {
         backgroundColor: Color(0xFF0001cf),
       ),
       body: ListView(
-        padding: EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(screenWidth * 0.03),
         children: [
-          ExerciseItem(
+          buildExerciseCard(
             exerciseImage: 'assets/images/exercise1.png',
             exerciseName: 'EXERCISE I',
             exerciseDescription: HEAD,
+            screenWidth: screenWidth,
+            screenHeight: screenHeight,
           ),
-          SizedBox(height: 16.0),
-          ExerciseItem(
+          SizedBox(height: screenHeight * 0.02),
+          buildExerciseCard(
             exerciseImage: 'assets/images/exercise2.png',
             exerciseName: 'EXERCISE II',
             exerciseDescription: CHEST,
+            screenWidth: screenWidth,
+            screenHeight: screenHeight,
           ),
-          SizedBox(height: 16.0),
-          ExerciseItem(
+          SizedBox(height: screenHeight * 0.02),
+          buildExerciseCard(
             exerciseImage: 'assets/images/exercise3.png',
             exerciseName: 'EXERCISE III',
-            exerciseDescription: STOMACH ,
+            exerciseDescription: STOMACH,
+            screenWidth: screenWidth,
+            screenHeight: screenHeight,
           ),
-          SizedBox(height: 16.0),
-          ExerciseItem(
+          SizedBox(height: screenHeight * 0.02),
+          buildExerciseCard(
             exerciseImage: 'assets/images/exercise4.png',
             exerciseName: 'EXERCISE IV',
             exerciseDescription: TRUNK,
+            screenWidth: screenWidth,
+            screenHeight: screenHeight,
           ),
-          SizedBox(height: 16.0),
-          ExerciseItem(
+          SizedBox(height: screenHeight * 0.02),
+          buildExerciseCard(
             exerciseImage: 'assets/images/exercise5.png',
             exerciseName: 'EXERCISE V',
             exerciseDescription: LOWER,
+            screenWidth: screenWidth,
+            screenHeight: screenHeight,
           ),
-          SizedBox(height: 16.0),
-          ExerciseItem(
+          SizedBox(height: screenHeight * 0.02),
+          buildExerciseCard(
             exerciseImage: 'assets/images/exercise6.png',
             exerciseName: 'EXERCISE VI',
             exerciseDescription: LEGS,
+            screenWidth: screenWidth,
+            screenHeight: screenHeight,
           ),
         ],
       ),
     );
   }
-}
 
-class ExerciseItem extends StatelessWidget {
-  final String exerciseImage;
-  final String exerciseName;
-  final String exerciseDescription;
-
-  ExerciseItem({
-    this.exerciseImage,
-    this.exerciseName,
-    this.exerciseDescription,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final double screenWidth = MediaQuery.of(context).size.width;
-    final double screenHeight = MediaQuery.of(context).size.height;
-
+  Widget buildExerciseCard({
+    String exerciseImage,
+    String exerciseName,
+    String exerciseDescription,
+    double screenWidth,
+    double screenHeight,
+  }) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -89,29 +120,25 @@ class ExerciseItem extends StatelessWidget {
           color: Color(0xFF0001cf),
           child: ListTile(
             leading: Image.asset(
-              'assets/icons/exercise.png', // Replace with your image asset path
-              width: 30, // Adjust image size
+              'assets/icons/exercise.png',
+              width: 30,
               height: 30,
             ),
             title: Text(
               exerciseName,
-              style: TextStyle(
-                fontSize: screenWidth * 0.05,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
+              style: flagTitleStyle,
             ),
           ),
         ),
-        SizedBox(height: 16.0),
+        SizedBox(height: screenHeight * 0.02),
         Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.03),
           child: ClipRRect(
             borderRadius: BorderRadius.circular(10),
             child: Image.asset(
               exerciseImage,
-              width: MediaQuery.of(context).size.width - 32.0,
-              height: MediaQuery.of(context).size.width * 0.6,
+              width: screenWidth - screenWidth * 0.06,
+              height: screenWidth * 0.6,
               fit: BoxFit.cover,
             ),
           ),
@@ -121,7 +148,7 @@ class ExerciseItem extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height: 16.0),
+              SizedBox(height: screenHeight * 0.02),
               Padding(
                 padding: const EdgeInsets.only(left: 24.0),
                 child: RichText(
@@ -129,14 +156,7 @@ class ExerciseItem extends StatelessWidget {
                     children: exerciseDescription.split('#').map((line) {
                       return TextSpan(
                         text: line,
-                        style: TextStyle(
-                          fontSize: screenWidth * 0.048,
-                          fontFamily: 'Lora',
-                          fontWeight: FontWeight.w900,
-                          letterSpacing: 1.5,
-                          color: Color(0xFF0001cf),
-                          height: 1.5,
-                        ),
+                        style: flagDescriptionStyle,
                       );
                     }).toList(),
                   ),
@@ -187,9 +207,3 @@ final String LEGS = '''
 # 
 # • Stand at attention, place your hands on your hips, stand on tiptoe, turn your knees outwards, and slowly bend them down to a squatting position while keeping your heels off the ground. Gradually raise your body back to the starting position. Repeat 12 times, inhaling as your body rises and exhaling as your body sinks.
 ''';
-
-void main() {
-  runApp(MaterialApp(
-    home: Exercises(),
-  ));
-}
