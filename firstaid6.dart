ba1f4d8315b32a4dd740477f03c6cfdc9c6d680a
SIVASUBRAMANIAN R <54441582+SiVa1710@users.noreply.github.com>
2024-05-05 15:20:33 +0530

    Update firstaid6.dart

--- firstaid6.dart
+++ firstaid6.dart
@@ -12,6 +12,7 @@ class FirstAid5 extends StatelessWidget {
         title: Text(
           'FIRST AID',
           style: TextStyle(
+            color: Colors.white,
             fontWeight: FontWeight.bold,
             fontSize: 20,
             fontFamily: 'Sarabun',
@@ -20,6 +21,7 @@ class FirstAid5 extends StatelessWidget {
         ),
         centerTitle: true,
         backgroundColor: Color(0xFF0001cf),
+        iconTheme: IconThemeData(color: Colors.white, size: 25),
       ),
       body: ListView(
         padding: EdgeInsets.all(screenWidth * 0.03),
@@ -112,10 +114,10 @@ class FirstAid5 extends StatelessWidget {
   }
 
   Widget buildFirstAidCard({
-    String name,
-    String description,
-    double screenWidth,
-    double screenHeight,
+    required String name,
+    required String description,
+    required double screenWidth,
+    required double screenHeight,
   }) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
